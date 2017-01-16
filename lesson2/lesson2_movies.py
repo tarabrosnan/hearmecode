81e11ce5dfbe4e36997b6ead5d73d2c35f59b95c
Tara Brosnan <brosnan.tara@gmail.com>
2017-01-16 15:10:26 -0500

    Revert "add to lesson1"
    This reverts commit f640410bb43b05238ee99db00da7291d66eaa4ca.

--- lesson2/lesson2_movies.py
+++
@@ -1,8 +0,0 @@
-movie_title = ['The Shawshank Redemption', 'The Godfather', 'The Godfather: Part II', 'The Dark Knight', '12 Angry Men']
-movie_rating = ['R', 'R', 'R', 'PG-13', 'Approved']
-bechdel_rating = [0, 2, 2, 3, 0]
-imdb_rating = [9.3, 9.2, 9.0, 9.0, 8.9]
-imdb_genre = ['Crime/Drama', 'Crime/Drama', 'Crime/Drama', 'Crime/Drama', 'Crime/Drama']
-
-for movie_title, movie_rating, bechdel_rating, imdb_rating, imdb_genre in zip(movie_title, movie_rating, bechdel_rating, imdb_rating, imdb_genre):
-	print "{0},{1},{2},{3},{4}".format(movie_title, movie_rating, bechdel_rating, imdb_rating, imdb_genre)
